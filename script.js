c4a062205064b04cad35fd100d9dc38c037751b9
azizjalilov <lukaharlow003@gmail.com>
2025-09-01 00:11:21 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Matn ustiga hover qilinganda neon effekt
-const title = document.querySelector(".title");
-title.addEventListener("mouseover", () => {
-  title.style.textShadow = "0 0 20px cyan, 0 0 40px blue";
-});
-title.addEventListener("mouseout", () => {
-  title.style.textShadow = "none";
-});
